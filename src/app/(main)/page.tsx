95a0cf1668e9ea607e2ee4af6533d7384f5799f3
Jamsjz <gj984233@gmail.com>
2025-08-05 23:53:48 +0545

    fix: Adjust spacing between navbar, hero, and event banner

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -118,7 +118,7 @@ export default function HomePage() {
 	const events = getAllFrontMatter("events");
 
 	return (
-		<main className="flex flex-col gap-20 md:gap-32 animate__animated animate__fadeIn">
+		<main className="flex flex-col gap-15 md:gap-16 animate__animated animate__fadeIn">
 			<section>
 				<Hero /> {/* Add the Hero component here */}
 			</section>
